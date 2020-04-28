e2e2ac93a2af7c897073678fec5e895427539f0c
finnhe <finn.he@jkopay.com>
2020-04-28 13:53:27 +0800

    add next slot

--- HashMap/HashMap.playground/Contents.swift
+++ HashMap/HashMap.playground/Contents.swift
@@ -39,12 +39,59 @@ protocol HashMapProtocol {
 }
 
 class HashMap {
-    private var array = Array<LinkedList<KeyValue>?>(repeating: nil, count: 256)
-    private let mod = 256
-    
-    private func mod(_ input: String) -> Int {
-        return Int(input.ascIISum) % Int(mod)
+    static let Mod = 256
+    static func mod(_ input: String) -> Int {
+        return Int(input.ascIISum) % Int(HashMap.Mod)
     }
+}
+
+class HashMapNextSlot {
+    private var array = Array<KeyValue?>(repeating: nil, count: HashMap.Mod)
+    
+}
+
+extension HashMapNextSlot: HashMapProtocol {
+    func get(_ key: String) -> String? {
+        var isStop = false
+        var currentIndex = HashMap.mod(key)
+        while !isStop {
+            if currentIndex > HashMap.Mod { return nil }
+            guard let target = array[currentIndex] else { return nil }
+            if target.key == key {
+                isStop = true
+                return target.value
+            }
+            currentIndex = currentIndex + 1
+         }
+    }
+    
+    func set(key: String, value: String) {
+        var isStop = false
+        var currentIndex = HashMap.mod(key)
+        while !isStop {
+            if currentIndex > HashMap.Mod { return }
+            guard let target = array[currentIndex] else {
+                array[currentIndex] = KeyValue(key: key, value: value)
+                isStop = true
+                break
+            }
+            if target.key == key {
+                target.value = value
+                isStop = true
+                break
+            }
+            currentIndex = currentIndex + 1
+        }
+        
+    }
+        
+    
+}
+
+
+
+class HashMapLinkList {
+    private var array = Array<LinkedList<KeyValue>?>(repeating: nil, count: HashMap.Mod)
     
     private func find(key: String, node: Node<KeyValue>) -> KeyValue? {
         guard let nodeValue = node.value else { return nil }
@@ -59,9 +106,9 @@ class HashMap {
     }
 }
 
-extension HashMap: HashMapProtocol {
+extension HashMapLinkList: HashMapProtocol {
     func get(_ key: String) -> String? {
-        let keyMod = mod(key)
+        let keyMod = HashMap.mod(key)
         guard let linklist = array[keyMod] else { return nil }
         guard let targetKeyValue = find(key: key, node: linklist.head) else { return nil }
         
@@ -69,7 +116,7 @@ extension HashMap: HashMapProtocol {
     }
     
     func set(key: String, value: String) {
-        let keyMod = mod(key)
+        let keyMod = HashMap.mod(key)
         guard let linklist = array[keyMod] else {
             let linkList = LinkedList<KeyValue>()
             let keyValue = KeyValue(key: key, value: value)
@@ -92,9 +139,13 @@ extension HashMap: HashMapProtocol {
 }
 
 class HashMapTests: XCTestCase {
-
+    var hashMap: HashMapProtocol!
+    override func setUp() {
+//        hashMap = HashMapLinkList()
+        hashMap = HashMapNextSlot()
+    }
+    
     func testSetAndGet() {
-        let hashMap = HashMap()
         hashMap.set(key: "ab", value: "Finn")
         hashMap.set(key: "ba", value: "Lex")
         hashMap.set(key: "a", value: "Jimmy")
@@ -110,11 +161,18 @@ class HashMapTests: XCTestCase {
     }
     
     func testCollision() {
-        let hashMap = HashMap()
-        hashMap.set(key: "ab", value: "Finn")
-        hashMap.set(key: "ba", value: "Lex")
-        XCTAssertEqual(hashMap.get("ab"), "Finn")
-        XCTAssertEqual(hashMap.get("ba"), "Lex")
+        hashMap.set(key: "abc", value: "Finn")
+        hashMap.set(key: "bac", value: "Lex")
+        hashMap.set(key: "cba", value: "Cuhck")
+
+        XCTAssertEqual(hashMap.get("abc"), "Finn")
+        XCTAssertEqual(hashMap.get("bac"), "Lex")
+        XCTAssertEqual(hashMap.get("cba"), "Cuhck")
+
+    }
+    
+    func testGetEmpty() {
+        XCTAssertEqual(hashMap.get("ba"), nil)
     }
     
 }
